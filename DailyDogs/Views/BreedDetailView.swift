e26af8e5631757f2480048548e05da3162d0dd3e
Ramona Cvelf <ramona.cvelf@gmail.com>
2021-03-31 18:52:40 +0200

    Initial Commit outlining UI-Structure

---
+++ DailyDogs/Views/BreedDetailView.swift
@@ -0,0 +1,26 @@
+//
+//  BreedDetailView.swift
+//  DailyDogs
+//
+//  Created by Ramona Cvelf on 31.03.21.
+//
+
+import Foundation
+import UIKit
+
+class BreedDetailView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+    }
+    
+    func configure(_ breed: Breed) {
+    }
+}
---
+++ DailyDogs/Views/BreedfeedController.swift
@@ -0,0 +1,23 @@
+//
+//  BreedfeedController.swift
+//  DailyDogs
+//
+//  Created by Ramona Cvelf on 31.03.21.
+//
+
+import Foundation
+import UIKit
+
+enum Section {
+    case allbreeds
+}
+
+class BreedfeedController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+    }
+}
